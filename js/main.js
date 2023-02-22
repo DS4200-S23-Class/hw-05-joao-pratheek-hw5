5c1528830f27560179b4f450ccabf45982da1ad8
JoaoPedro-dot <castro.joa@northeastern.edu>
2023-02-22 15:38:34 -0500

    New changes 2/22

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ Modified: 02/21/2023
 */
 
 // First, we need a frame  
-const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500; 
+const FRAME_HEIGHT = 600;
+const FRAME_WIDTH = 600; 
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
 // Let's do another example, with a scale 
@@ -14,7 +14,7 @@ const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
 const FRAME1 = d3.select("#vis1")
-                  .append("svg")
+                    .append("svg")
                     .attr("height", FRAME_HEIGHT)
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
@@ -182,28 +182,98 @@ const addButton = rightColumn.append("button")
 
 addButton.on("click", handleAddPoint);
 
-
-
-// New frame  
-const FRAME_HEIGHT2 = 500;
-const FRAME_WIDTH2 = 500; 
-const MARGINS2 = {left: 50, right: 50, top: 50, bottom: 50};
-
-// Let's do another example, with a scale 
-const VIS_HEIGHT2 = FRAME_HEIGHT2 - MARGINS2.top - MARGINS2.bottom;
-const VIS_WIDTH2 = FRAME_WIDTH2 - MARGINS2.left - MARGINS2.right; 
-
+// new FRAME
 const FRAME2 = d3.select("#vis2")
                   .append("svg")
-                    .attr("height", FRAME_HEIGHT2)
-                    .attr("width", FRAME_WIDTH2)
-                    .attr("class", "frame"); 
-
-// builds scatter plot
-function build_interative_bargraph() {
+                  .attr("height", FRAME_HEIGHT)
+                  .attr("width", FRAME_WIDTH)
+                  .attr("class", "frame"); 
+                
+// builds bar graph
+function build_interative_bar_graph() {
 
 // read data and create plot
-d3.csv("data/bar-data.csv").then((data) => {
+d3.csv("data/bar-data.csv").then((data2) => {
 
-});
+  // find max X
+  const MAX_X2 = d3.max(data2, (d) => d.category);
+          // Note: data read from csv is a string, so you need to
+          // cast it to a number if needed 
+
+  // find max Y
+  const MAX_Y2 = d3.max(data2, (d) => { return parseInt(d.amount); });
+  
+  // Define scale functions that maps our data values 
+  // (domain) to pixel values (range)
+  const X_SCALE2 = d3.scaleBand() 
+                    .domain(["A", "B", "C", "D", "E", "F", "G"])
+                    .range([0, VIS_WIDTH])
+                    .padding(0.2);
+
+  const Y_SCALE2 = d3.scaleLinear() 
+                    .domain([MAX_Y2, 0]) 
+                    .range([0, VIS_HEIGHT]); 
+
+  // Use X_SCALE to plot our points
+  FRAME2.selectAll("rect")  
+      .data(data2)
+      .enter()       
+      .append("rect")  
+        .attr("x", (d) => { return (X_SCALE2(d.category) + MARGINS.left); }) 
+        .attr("y", (d) => { return (Y_SCALE2(d.amount) + MARGINS.top); }) 
+        .attr("width", X_SCALE2.bandwidth())
+        .attr("height", (d) => VIS_HEIGHT - Y_SCALE2(parseInt(d.amount)))
+        .attr("fill", "dodgerblue");
+
+  // Tooltip
+
+    // To add a tooltip, we will need a blank div that we 
+    // fill in with the appropriate text. Be use to note the
+    // styling we set here and in the .css
+    const TOOLTIP = d3.select("#vis2")
+                        .append("div")
+                          .attr("class", "tooltip")
+                          .style("opacity", 0); 
+
+    // Define event handler functions for tooltips
+    function handleMouseover(event, d) {
+      d3.select(this)
+        .attr("fill", "orange");
+    }
+
+    function handleMousemove(event, d) {
+      // position the tooltip and fill in information 
+      TOOLTIP.html("Category: " + d.category + "<br>Amonut: " + d.amount)
+              .style("left", page.X + "px")
+              .style("top", page.Y + "px"); 
+    }
+
+    function handleMouseleave(event, d) {
+      d3.select(this)
+        .attr("fill", "dodgerblue");
+    } 
+
+  // Add event listeners
+  FRAME2.selectAll("rect")
+        .on("mouseover", handleMouseover) //add event listeners
+        .on("mousemove", handleMousemove)
+        .on("mouseleave", handleMouseleave);    
+
+  // Add an X-axis to the vis  
+  FRAME2.append("g") 
+        .attr("transform", "translate(" + MARGINS.left + 
+              "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+        .call(d3.axisBottom(X_SCALE2)) 
+          .attr("font-size", '20px');
+
+  // Add Y-axis to the vis  
+  FRAME2.append("g") 
+        .attr("transform", "translate(" + MARGINS.left + 
+              "," + MARGINS.top + ")") 
+        .call(d3.axisLeft(Y_SCALE2)) 
+          .attr("font-size", '20px'); 
+  });
 }
+
+// Call function 
+build_interative_bar_graph();
