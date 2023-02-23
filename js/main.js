ddaeab90c2e8485e92d174ca5003a1c8dc20cfb1
JoaoPedro-dot <castro.joa@northeastern.edu>
2023-02-23 14:17:50 -0500

    Final Updates 2/23

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*
 D3 Code 
 Joao & Pratheek 
-Modified: 02/21/2023
+Modified: 02/23/2023
 */
 
 // dimensions of the frame  
@@ -15,8 +15,7 @@ const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
-// selects the vis1 element, appends an svg element
-// and gives it attributes
+// selects the vis1 element and appends an svg element
 const FRAME1 = d3.select("#vis1")
                     .append("svg")
                     .attr("height", FRAME_HEIGHT)
@@ -29,22 +28,18 @@ function build_interative_scatter() {
 // reads data from the csv file
 d3.csv("data/scatter-data.csv").then((data) => {
 
-  // finds max X value in the csv data
-  // cast to int type because it's a string in the csv
+  // finds max X and Y values in the csv data
+  // cast to int type when string in the csv
   const MAX_X = d3.max(data, (d) => { return parseInt(d.x); });
 
-  // finds max Y value in the csv data
-  // cast to int type because it's a string in the csv
   const MAX_Y = d3.max(data, (d) => { return parseInt(d.y); });
   
-  // defines scale function that maps X data values 
+  // defines scale function that maps X and Y data values 
   // (domain) to pixel values (range)
   const X_SCALE = d3.scaleLinear() 
                     .domain([0, 10])
                     .range([0, VIS_WIDTH]); 
 
-  // defines scale function that maps Y data values 
-  // (domain) to pixel values (range)
   const Y_SCALE = d3.scaleLinear() 
                     .domain([10, 0]) 
                     .range([0, VIS_HEIGHT]); 
@@ -57,6 +52,7 @@ d3.csv("data/scatter-data.csv").then((data) => {
         .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left); }) 
         .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.top); })  
         .attr("r", 10)
+        .attr("fill", "dodgerblue")
         .attr("class", "point");
 
   // adds event listeners
@@ -65,19 +61,18 @@ d3.csv("data/scatter-data.csv").then((data) => {
           .on("mouseleave", handleMouseleave)
           .on("click", handleClick);
 
-  // adds an X-axis to the vis  
+  // adds an X and Y axis to the vis  
   FRAME1.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (VIS_HEIGHT + MARGINS.top) + ")") 
         .call(d3.axisBottom(X_SCALE)) 
-          .attr("font-size", '20px');
+          .attr("font-size", '15px');
 
-  // adds a Y-axis to the vis  
   FRAME1.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + MARGINS.top + ")") 
         .call(d3.axisLeft(Y_SCALE)) 
-          .attr("font-size", '20px'); 
+          .attr("font-size", '15px'); 
   });
 }
 
@@ -93,15 +88,20 @@ function handleMouseover(event, d) {
 // defines event handler function for a mouse removal
 function handleMouseleave(event, d) {
   d3.select(this)
-    .attr("fill", "black");
+    .attr("fill", "dodgerblue");
 }
 
 // defines event handler function for a mouse click
 function handleClick(event, d) {
   const circle = d3.select(this);
   const border = circle.attr("stroke-width") == "5";
-  circle.attr("stroke-width", border ? "0" : "5")
-        .attr("stroke", border ? "none" : "red");
+  if (border) {
+    circle.attr("stroke-width", "0");
+    circle.attr("stroke", "none");
+  } else {
+    circle.attr("stroke-width", "5");
+    circle.attr("stroke", "red");
+  }
 
   // updates text with coordinates of circle that was last clicked
   d3.select("#previous-click").text("Last Point Clicked: (" + d.x + "," + d.y + ")");
@@ -111,25 +111,21 @@ function handleClick(event, d) {
 const rightColumn = d3.select(".right-column");
 
 // appends the paragraph element to the right-column element
-// gives it an id, text, and a 20px margin from the bottom
 rightColumn.append("p")
   .attr("id", "previous-click")
   .text("Last Point Clicked: ")
   .style("margin-bottom", "20px");
 
 // appends the form element to the right-column element and stores it as a constant
-// gives it an id
 const form = rightColumn.append("form")
   .attr("id", "coordinates");
 
 // appends the label element to the form element and stores it as a constant
-// and gives it an attribute value to match the select tag name and a text
 const xLabel = form.append("label")
   .attr("for", "x-coordinate")
   .text("Select the x-coordinate for the point you want to add:");
 
 // appends the select element to the form element and stores it as a constant
-// gives it an id and name
 const xSelect = form.append("select")
   .attr("id", "x-value")
   .text("name", "x-coordinate");
@@ -144,18 +140,15 @@ xSelect.selectAll("option")
     .text((d, i) => i+1);
 
 // appends the paragraph element to the form element
-// and gives it a 20px margin from the bottom
 form.append("p")
   .style("margin-bottom", "20px");
 
 // appends the label element to the form element and stores it as a constant
-// and gives it an attribute value to match the select tag name and a text
 const yLabel = form.append("label")
   .attr("for", "y-coordinate")
   .text("Select the y-coordinate for the point you want to add:");
 
 // appends the select element to the form element and stores it as a constant
-// gives it an id, name, and a 20px margin from the bottom
 const ySelect = form.append("select")
   .attr("id", "y-value")
   .text("name", "y-coordinate")
@@ -173,6 +166,8 @@ ySelect.selectAll("option")
 // defines event handler for adding a new point to the scatter
 function handleAddPoint() {
 
+  // re-defines scale function that maps X and Y data values 
+  // (domain) to pixel values (range)
   const X_SCALE = d3.scaleLinear() 
                     .domain([0, 10])
                     .range([0, VIS_WIDTH]); 
@@ -188,23 +183,26 @@ function handleAddPoint() {
   // creates a constant that has properties of X and Y coordinates
   const newData = { x: xCoord, y: yCoord };
 
-  // appends a new circle to FRAME1
-  // and passes
+  // appends the new circle to FRAME1
   FRAME1.append("circle")
         .attr("cx", X_SCALE(newData.x) + MARGINS.left) 
         .attr("cy", Y_SCALE(newData.y) + MARGINS.top)  
         .attr("r", 10)
+        .attr("fill", "dodgerblue")
         .attr("class", "point")
           .on("mouseover", handleMouseover)
           .on("mouseleave", handleMouseleave)
           .on("click", function(event, d) { // rewriting the handleClick function
                                             // to redefine constants
-            
             const circle = d3.select(this);
             const border = circle.attr("stroke-width") == "5";
-            
-            circle.attr("stroke-width", border ? "0" : "5")
-                  .attr("stroke", border ? "none" : "red");
+            if (border) {
+              circle.attr("stroke-width", "0");
+              circle.attr("stroke", "none");
+            } else {
+              circle.attr("stroke-width", "5");
+              circle.attr("stroke", "red");
+            }
 
             // updates text with coordinates of new circle that was last clicked
             d3.select("#previous-click").text("Last Point Clicked: (" + newData.x + "," + newData.y + ")");
@@ -212,7 +210,6 @@ function handleAddPoint() {
 }
 
 // appends button element to the right-column element and stores it as a constant
-// gives it an id and text
 const button = rightColumn.append("button")
   .attr("id", "button")
   .text("Add Point");
@@ -220,8 +217,7 @@ const button = rightColumn.append("button")
 // adds event listener for a mouse click on the button
 button.on("click", handleAddPoint);
 
-// selects the vis1 element, appends an svg element
-// and gives it attributes
+// selects the vis1 element and appends an svg element
 const FRAME2 = d3.select("#vis2")
                   .append("svg")
                   .attr("height", FRAME_HEIGHT)
@@ -235,18 +231,16 @@ function build_interative_bar_graph() {
 d3.csv("data/bar-data.csv").then((data2) => {
 
   // finds max Y value in the csv data
-  // cast to int type because it's a string in the csv
+  // cast to int type when string in the csv
   const MAX_Y2 = d3.max(data2, (d) => { return parseInt(d.amount); });
   
-  // defines scale function that maps X data values 
+  // defines scale function that maps X and Y data values 
   // (domain) to pixel values (range)
   const X_SCALE2 = d3.scaleBand() 
                     .domain(["A", "B", "C", "D", "E", "F", "G"])
                     .range([0, VIS_WIDTH])
                     .padding(0.2);
 
-  // defines scale function that maps Y data values 
-  // (domain) to pixel values (range)
   const Y_SCALE2 = d3.scaleLinear() 
                     .domain([100, 0]) 
                     .range([0, VIS_HEIGHT]); 
@@ -262,8 +256,7 @@ d3.csv("data/bar-data.csv").then((data2) => {
         .attr("height", (d) => VIS_HEIGHT - Y_SCALE2(parseInt(d.amount)))
         .attr("fill", "dodgerblue");
 
-    // creates a new constant, selects the vis2 element, appends a div, 
-    // and adds a class tooltip
+    // selects the vis2 element and adds a class tooltip
     const TOOLTIP = d3.select("#vis2")
                         .append("div")
                           .attr("class", "tooltip"); 
@@ -295,19 +288,18 @@ d3.csv("data/bar-data.csv").then((data2) => {
         .on("mousemove", handleMousemove)
         .on("mouseleave", handleMouseleave);    
 
-  // adds an X-axis to the vis  
+  // adds an X and Y axis to the vis  
   FRAME2.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (VIS_HEIGHT + MARGINS.top) + ")") 
         .call(d3.axisBottom(X_SCALE2)) 
-          .attr("font-size", '20px');
+          .attr("font-size", '15px');
 
-  // adds a Y-axis to the vis  
   FRAME2.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + MARGINS.top + ")") 
         .call(d3.axisLeft(Y_SCALE2)) 
-          .attr("font-size", '20px'); 
+          .attr("font-size", '15px'); 
   });
 }
 
